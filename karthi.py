f4b04b9fe36e7c48ff19c8571c007ffb87ae06c9
bagavath <karthicksriram112.com>
2023-08-30 03:50:39 -0700

    ramya

---
+++ karthi.py
@@ -0,0 +1,56 @@
+# modules date time day 2
+"""""
+import randomimport datetime
+a=datetime.datetime.now()
+print(a)
+
+import datetime
+a=datetime.datetime(2023,7,16)
+print(a)
+
+import datetime
+a=datetime.datetime.now()
+print(a.year)
+print(a.month)
+print(a.day)
+print(a.strftime("%a"))
+print(a.strftime("%A"))
+random.random()
+print(random.randint(4,7))
+
+num=int(input("enter the odd number :"))
+if(num%3)==0:
+    print("odd number")
+else:
+    print("even number",)
+
+import random
+a=random.random()
+print(x)
+
+
+
+#task
+kilometer=int(input("enter the number :"))
+conv=0.621371
+miles=kilometer*conv
+print(miles)
+
+celsius=int(input("enter the number :"))"""
+
+
+#day 3 modules
+
+#request
+
+import request
+x=requests.get("https://w3.com/")
+print(x.get)
+
+
+
+
+
+
+
+
